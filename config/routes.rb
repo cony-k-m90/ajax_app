6f8c73d0e4f886cd61f815d2cabe945bd615d5c1
cony-k-m90 <78285637+cony-k-m90@users.noreply.github.com>
2021-04-12 00:11:05 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  root to: 'posts#index' # <- 編集
+  # get 'posts/new', to: 'posts#new' <-削除(「投稿完了しました」ページは使用しない為)
   post 'posts', to: 'posts#create'
 end
