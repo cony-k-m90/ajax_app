296852926255a8f901e0dc83446a612c5fb27f2c
cony-k-m90 <78285637+cony-k-m90@users.noreply.github.com>
2021-04-13 20:30:33 +0900

    既読機能のフロントエンド実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'posts#index' # <- 編集
   # get 'posts/new', to: 'posts#new' <-削除(「投稿完了しました」ページは使用しない為)
   post 'posts', to: 'posts#create'
-  get 'posts', to: '#posts#checked' #既読機能のエンドポイントをqueryパラメータを使用して記述
+  get 'posts/:id', to: 'posts#checked'
 end
